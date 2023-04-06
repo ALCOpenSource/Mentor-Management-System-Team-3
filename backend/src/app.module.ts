66a029cebce55ecb21059d6803a0d409df0cbbf6
ikechukwu-peter <pitiyke@gmail.com>
2023-04-06 07:30:34 +0100

    initial commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
