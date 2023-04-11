c9f9e70baedb5fdb248ffeba878440dfd1e43ef0
ikechukwu-peter <pitiyke@gmail.com>
2023-04-11 10:40:24 +0100

    fix: missing import for cloudinary

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -5,13 +5,14 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { UsersController } from "./users.controller";
 import { User, UserSchema } from "./users.schema";
 import { FirebaseModule } from "../firebase/firebase.module";
+import { CloudinaryService } from "../cloudinary/cloudinary.service";
 
 @Module({
   imports: [
     FirebaseModule,
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  providers: [UsersService],
+  providers: [UsersService, CloudinaryService],
   controllers: [UsersController],
 })
 export class UsersModule {}
