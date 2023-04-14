33e667f9f721820aa165b0f17607d6ff355c0423
Esther-Wavinya <estherwavinya80@gmail..com>
2023-04-14 13:21:55 +0300

    navbar and sidebar

--- frontend/mms-Admin/src/views/dashboard/dashboard.tsx
+++ frontend/mms-Admin/src/views/dashboard/dashboard.tsx
@@ -19,36 +19,3 @@ function Dashboard() {
 export default Dashboard;
 
 
-  // const [user, loading] = useAuthState(auth);
-  // const [name, setName] = useState("");
-  // const navigate = useNavigate();
-  // const fetchUserName = async () => {
-  //   try {
-  //     const q = query(collection(db, "users"), where("uid", "==", user?.uid));
-  //     const doc = await getDocs(q);
-  //     const data = doc.docs[0].data();
-  //     setName(data.name);
-  //   } catch (err) {
-  //     console.error(err);
-  //     alert("An error occured while fetching user data");
-  //   }
-  // };
-  // // eslint-disable-next-line react-hooks/exhaustive-deps
-  // useEffect(() => {
-  //   if (loading) return;
-  //   if (!user) return navigate("/");
-  //   fetchUserName();
-  // }, [user, loading]);
-  
-  // return (
-  //   <div className="dashboard">
-  //      <div className="dashboard__container">
-  //       Logged in as
-  //        <div>{name}</div>
-  //        <div>{user?.email}</div>
-  //        <button className="dashboard__btn" onClick={logout}>
-  //         Logout
-  //        </button>
-  //      </div>
-  //    </div>
-  // );
