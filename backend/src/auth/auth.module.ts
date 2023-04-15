3c75250e5f32c7a682751a55437645144db58178
karani12 <karanijohn018@gmail.com>
2023-04-15 11:08:09 +0300

    chore:clean up

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -9,13 +9,10 @@ import {
   PreferencesSchema,
 } from "src/preferences/preferences.schema";
 import { UsersService } from "./../users/users.service";
-<<<<<<< HEAD
 import { CloudinaryModule } from "src/cloudinary/cloudinary.module";
 import { PreferencesModule } from "src/preferences/preferences.module";
 import { PreferencesService } from "src/preferences/preferences.service";
-=======
-import { CloudinaryService } from "../cloudinary/cloudinary.service";
->>>>>>> main
+import { CloudinaryService } from "src/cloudinary/cloudinary.service";
 
 @Module({
   imports: [
@@ -26,11 +23,7 @@ import { CloudinaryService } from "../cloudinary/cloudinary.service";
     CloudinaryModule,
     PreferencesModule,
   ],
-<<<<<<< HEAD
-  providers: [AuthService, UsersService, PreferencesService],
-=======
-  providers: [AuthService, UsersService, CloudinaryService],
->>>>>>> main
+  providers: [AuthService, UsersService, PreferencesService, CloudinaryService],
   controllers: [AuthController],
 })
 export class AuthModule {}
