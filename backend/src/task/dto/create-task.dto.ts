bad12b2c21b052dd4cb49f49c96ddeed6478cfbd
ikechukwu-peter <pitiyke@gmail.com>
2023-04-20 21:39:35 +0100

    feat: added 32 character constraint

--- backend/src/task/dto/create-task.dto.ts
+++ backend/src/task/dto/create-task.dto.ts
@@ -1,9 +1,10 @@
 import { Type } from "class-transformer";
-import { IsString, IsArray, IsNotEmpty } from "class-validator";
+import { IsString, IsArray, IsNotEmpty, MaxLength } from "class-validator";
 
 export class CreateTaskDto {
   @IsString()
   @IsNotEmpty()
+  @MaxLength(32)
   title: string;
 
   @IsString()
