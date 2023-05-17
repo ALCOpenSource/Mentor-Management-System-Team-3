7312f13539d595a5b91da3f3631a340a48cdd60a
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-05-17 23:33:50 +0100

    [feat] - About Page-Admin-App - Sprint 3 #26

--- frontend/mms-Admin/src/views/dashboard/SwitchComponents/mentor/config/mentors.ts
+++ frontend/mms-Admin/src/views/dashboard/SwitchComponents/mentor/config/mentors.ts
@@ -1,70 +1,70 @@
-import avatar from "../../../../../assets/images/mentors/user.jpg";
-import { Mentor, Tab } from "../types";
+import avatar from '../../../../../assets/images/mentors/user.jpg';
+import { Mentor, Tab } from '../types';
 
 export const MentorLists: Mentor[] = [
   {
     userImageLink: avatar,
-    name: "Kabiru Omo John",
-    createdDate: "Added 0ct. 10 2022",
+    name: 'Kabiru Omo James',
+    createdDate: 'Added 0ct. 10 2022',
   },
   {
     userImageLink: avatar,
-    name: "Kabiru Omo Isaka",
-    createdDate: "Added 0ct. 10 2022",
+    name: 'Kabiru Omo Isaka',
+    createdDate: 'Added 0ct. 10 2022',
   },
   {
     userImageLink: avatar,
-    name: "Kabiru Omo Isaka",
-    createdDate: "Added 0ct. 10 2022",
+    name: 'Kabiru Omo Isaka',
+    createdDate: 'Added 0ct. 10 2022',
   },
   {
     userImageLink: avatar,
-    name: "Kabiru Omo Isaka",
-    createdDate: "Added 0ct. 10 2022",
+    name: 'Kabiru Omo Isaka',
+    createdDate: 'Added 0ct. 10 2022',
   },
   {
     userImageLink: avatar,
-    name: "Kabiru Omo Isaka",
-    createdDate: "Added 0ct. 10 2022",
+    name: 'Kabiru Omo Isaka',
+    createdDate: 'Added 0ct. 10 2022',
   },
   {
     userImageLink: avatar,
-    name: "Kabiru Omo Isaka",
-    createdDate: "Added 0ct. 10 2022",
+    name: 'Kabiru Omo Isaka',
+    createdDate: 'Added 0ct. 10 2022',
   },
   {
     userImageLink: avatar,
-    name: "Kabiru Omo Isaka",
-    createdDate: "Added 0ct. 10 2022",
+    name: 'Kabiru Omo Isaka',
+    createdDate: 'Added 0ct. 10 2022',
   },
   {
     userImageLink: avatar,
-    name: "Kabiru Omo Isaka",
-    createdDate: "Added 0ct. 10 2022",
+    name: 'Kabiru Omo Isaka',
+    createdDate: 'Added 0ct. 10 2022',
   },
   {
     userImageLink: avatar,
-    name: "Kabiru Omo Isaka",
-    createdDate: "Added 0ct. 10 2022",
+    name: 'Kabiru Omo Isaka',
+    createdDate: 'Added 0ct. 10 2022',
   },
   {
     userImageLink: avatar,
-    name: "Kabiru Omo Isaka",
-    createdDate: "Added 0ct. 10 2022",
+    name: 'Kabiru Omo Isaka',
+    createdDate: 'Added 0ct. 10 2022',
   },
 ];
 
 export const Tabs: Tab[] = [
   {
-    lable: "About",
+    lable: 'About',
   },
   {
-    lable: "Programs",
+    lable: 'Programs',
   },
   {
-    lable: "Tasks",
+    lable: 'Tasks',
   },
   {
-    lable: "Certificates",
+    lable: 'Certificates',
   },
 ];
